56fb2638f844cc752af512fb3946408e974dc167
rahdnus <rahdnus119@gmail.com>
2022-11-10 16:59:03 +0530

    camera

--- src/main.cpp
+++ src/main.cpp
@@ -31,19 +31,23 @@ int main() {
     const int samples_per_pixel = 100;
     const int max_depth=50;
 
-    hittable_list world;
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(0.8);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+hittable_list world;
 
-    world.add(make_shared<Sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<Sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<Sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<Sphere>(point3(-1.0,    0.0, -1.0),  -0.48, material_left));
-    world.add(make_shared<Sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+auto material_left   = make_shared<dielectric>(1.5);
+auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-    camera cam;
+world.add(make_shared<Sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+world.add(make_shared<Sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+world.add(make_shared<Sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+world.add(make_shared<Sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
+world.add(make_shared<Sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+
+camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 90, aspect_ratio);
+    // Camera
+
+
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
